6e011cfb29e4780e36a3d95f9b9449a0fddd13e5
Manuel Kollus <officialragecode@gmail.com>
2019-01-26 00:13:03 +0100

    Migrating java.io.File to java.nio.Path

--- core/src/main/java/com/github/impulsecl/impulse/core/service/ServiceIndexLoader.java
+++ core/src/main/java/com/github/impulsecl/impulse/core/service/ServiceIndexLoader.java
@@ -1,11 +1,13 @@
 package com.github.impulsecl.impulse.core.service;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.ArrayList;
@@ -28,64 +30,61 @@ final class ServiceIndexLoader {
 
   @NonNull
   Collection<ServiceIndexRecord> loadServices() {
-    File servicesDirectory = new File("services");
-
-    if (!servicesDirectory.exists()) {
-      boolean successfullyFileCreated = servicesDirectory.mkdir();
-
-      if (!successfullyFileCreated) {
-        LOGGER.error("Impulse does not have the permission to create a folder!");
-      }
-    }
-
-    File[] serviceJarFiles = servicesDirectory.listFiles(file -> file.getName().endsWith(".jar"));
     List<ServiceIndexRecord> loadedRecords = new ArrayList<>();
 
-    if (serviceJarFiles == null) {
-      return loadedRecords;
-    }
+    try {
+      if (Files.notExists(Paths.get("services"))) {
+        Path path = Files.createDirectory(Paths.get(""));
 
-    for (File serviceJarFile : serviceJarFiles) {
-      try (JarFile jarFile = new JarFile(serviceJarFile)) {
-        Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+        LOGGER.info("Creating the " + path.toFile().getName() + " file...");
 
-        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
-          URLClassLoader urlClassLoader;
+        Files.list(Paths.get("services")).forEach(servicePath -> {
 
-          try {
-            urlClassLoader = new URLClassLoader(new URL[]{serviceJarFile.toURI().toURL()});
-          } catch (MalformedURLException cause) {
-            throw new RuntimeException(cause);
+          try (JarFile jarFile = new JarFile(servicePath.toFile())) {
+            Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+
+            AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+              URLClassLoader urlClassLoader;
+
+              try {
+                urlClassLoader = new URLClassLoader(new URL[]{servicePath.toFile().toURI().toURL()});
+              } catch (MalformedURLException cause) {
+                throw new RuntimeException(cause);
+              }
+
+              while (jarEntryEnumeration.hasMoreElements()) {
+                JarEntry jarEntry = jarEntryEnumeration.nextElement();
+
+                if (jarEntry.isDirectory() || !jarEntry.getName().endsWith(".class")) {
+                  continue;
+                }
+
+                Class<?> clazz;
+                String targetClassName = jarEntry.getName()
+                    .substring(0, jarEntry.getName().length() - 6)
+                    .replace("/", ".");
+
+                try {
+                  clazz = urlClassLoader.loadClass(targetClassName);
+                } catch (ClassNotFoundException cause) {
+                  throw new RuntimeException(cause);
+                }
+
+                if (clazz.isAssignableFrom(Service.class)) {
+                  ServiceIndexRecord serviceIndexRecord = new ServiceIndexRecord(clazz, true);
+                  loadedRecords.add(serviceIndexRecord);
+                }
+              }
+
+              return null;
+            });
+          } catch (IOException cause) {
+            cause.printStackTrace();
           }
-
-          while (jarEntryEnumeration.hasMoreElements()) {
-            JarEntry jarEntry = jarEntryEnumeration.nextElement();
-
-            if (jarEntry.isDirectory() || !jarEntry.getName().endsWith(".class")) {
-              continue;
-            }
-
-            Class<?> clazz;
-            String targetClassName = jarEntry.getName()
-                .substring(0, jarEntry.getName().length() - 6)
-                .replace("/", ".");
-
-            try {
-              clazz = urlClassLoader.loadClass(targetClassName);
-            } catch (ClassNotFoundException cause) {
-              throw new RuntimeException(cause);
-            }
-
-            if (clazz.isAssignableFrom(Service.class)) {
-              ServiceIndexRecord serviceIndexRecord = new ServiceIndexRecord(clazz, true);
-              loadedRecords.add(serviceIndexRecord);
-            }
-          }
-          return null;
         });
-      } catch (IOException cause) {
-        cause.printStackTrace();
       }
+    } catch (IOException cause) {
+      cause.printStackTrace();
     }
 
     return loadedRecords;
