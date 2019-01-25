eb0fede0620cf2fcbebd2106a2a52a23358cc6d9
Manuel Kollus <officialragecode@gmail.com>
2019-01-25 23:54:48 +0100

    Implement an service class for the swarm

---
+++ swarm/src/main/java/com/github/impulsecl/impulse/swarm/SwarmService.java
@@ -0,0 +1,19 @@
+package com.github.impulsecl.impulse.swarm;
+
+import com.github.impulsecl.impulse.core.service.AbstractService;
+import com.github.impulsecl.impulse.core.service.annotation.ServiceMetadata;
+
+@ServiceMetadata(name = "Swarm", serviceCommand = "-s", description = "Communicate with the daemons and clients")
+public class SwarmService extends AbstractService {
+
+  @Override
+  public void start() {
+
+  }
+
+  @Override
+  public void stop() {
+
+  }
+
+}
