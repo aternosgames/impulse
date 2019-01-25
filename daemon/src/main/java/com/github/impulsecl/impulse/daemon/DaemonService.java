994ca003f218dd4fbfa521480c9764cb1db5be38
Manuel Kollus <officialragecode@gmail.com>
2019-01-25 23:06:25 +0100

    Adding an missing ServiceMetadata annotation

--- daemon/src/main/java/com/github/impulsecl/impulse/daemon/DaemonService.java
+++ daemon/src/main/java/com/github/impulsecl/impulse/daemon/DaemonService.java
@@ -1,7 +1,10 @@
 package com.github.impulsecl.impulse.daemon;
 
 import com.github.impulsecl.impulse.core.service.AbstractService;
+import com.github.impulsecl.impulse.core.service.annotation.ServiceMetadata;
 
+@ServiceMetadata(name = "Daemon", serviceCommand = "-d",
+    description = "Responsible service for deploy Minecraft servers using blueprints")
 public class DaemonService extends AbstractService {
 
   @Override
