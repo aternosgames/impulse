0af3ebd1475c842a961c1054c9c482a6d77e40e1
Manuel Kollus <officialragecode@gmail.com>
2019-01-25 23:03:12 +0100

    Implement an service class for the daemon

---
+++ daemon/src/main/java/com/github/impulsecl/impulse/daemon/DaemonService.java
@@ -0,0 +1,16 @@
+package com.github.impulsecl.impulse.daemon;
+
+import com.github.impulsecl.impulse.core.service.AbstractService;
+
+public class DaemonService extends AbstractService {
+
+  @Override
+  public void start() {
+
+  }
+
+  @Override
+  public void stop() {
+
+  }
+}
