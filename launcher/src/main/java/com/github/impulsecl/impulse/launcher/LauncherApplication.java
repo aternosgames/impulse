b54ffaa772bf52e522caf32175a54a21b53ddf90
Manuel Kollus <officialragecode@gmail.com>
2019-01-25 22:28:34 +0100

    Integration of the main class and the field "description" was added to the object.

---
+++ launcher/src/main/java/com/github/impulsecl/impulse/launcher/LauncherApplication.java
@@ -0,0 +1,60 @@
+package com.github.impulsecl.impulse.launcher;
+
+import com.github.impulsecl.impulse.core.service.Service;
+import com.github.impulsecl.impulse.core.service.ServiceIndex;
+import com.github.impulsecl.impulse.core.service.ServiceIndexRecord;
+import com.github.impulsecl.impulse.core.service.ServiceInvoker;
+import java.util.Optional;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class LauncherApplication {
+
+  private static final ServiceIndex SERVICE_INDEX = ServiceIndex.create();
+  private static final ServiceInvoker SERVICE_INVOKER = ServiceInvoker.create();
+
+  public static void main(String[] arguments) {
+    SERVICE_INDEX.registerRecordRecursive();
+
+    Options options = new Options();
+
+    for (ServiceIndexRecord serviceIndexRecord : SERVICE_INDEX.getRecords()) {
+      options.addOption(serviceIndexRecord.getServiceCommand(), false,
+          serviceIndexRecord.getDescription());
+    }
+
+    try {
+      CommandLineParser commandLineParser = new DefaultParser();
+      CommandLine commandLine = commandLineParser.parse(options, arguments);
+
+      for (ServiceIndexRecord serviceIndexRecord : SERVICE_INDEX.getRecords()) {
+        if (commandLine.hasOption(serviceIndexRecord.getServiceCommand())) {
+
+          Optional<Service> optionalService = SERVICE_INVOKER
+              .invokeService(serviceIndexRecord.getServiceClass());
+
+          if (optionalService.isPresent()) {
+            Service service = optionalService.get();
+            service.start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> service.stop()));
+
+            while (true) {
+              try {
+                Thread.sleep(500);
+              } catch (InterruptedException cause) {
+                cause.printStackTrace();
+              }
+            }
+          }
+        }
+      }
+    } catch (ParseException cause) {
+      cause.printStackTrace();
+    }
+  }
+
+}
