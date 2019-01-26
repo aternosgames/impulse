e7565a95bb9dd17fbe87eaf7ee1a0e0ae00f437e
Clockw1seLrd <clockw1selrd@gmail.com>
2019-01-26 02:26:34 +0100

    Omg

--- daemon/src/main/java/com/github/impulsecl/impulse/daemon/blueprint/config/BlueprintConfigProvider.java
+++ daemon/src/main/java/com/github/impulsecl/impulse/daemon/blueprint/config/BlueprintConfigProvider.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface BlueprintConfigProvider {
 
+  @NonNull
   @CheckReturnValue
   static BlueprintConfigProvider json() {
     return new JsonBlueprintConfigProvider();
