ec878d6239b2ef8794c4fef35d03dc3b4d8905b5
Manuel Kollus <officialragecode@gmail.com>
2019-01-28 22:17:01 +0100

    Remove unnecessary variables

--- core/src/main/java/com/github/impulsecl/impulse/core/gateway/compiler/StandardGatewayCompiler.java
+++ core/src/main/java/com/github/impulsecl/impulse/core/gateway/compiler/StandardGatewayCompiler.java
@@ -2,7 +2,6 @@ package com.github.impulsecl.impulse.core.gateway.compiler;
 
 import com.github.impulsecl.impulse.core.gateway.GatewayException;
 import com.github.impulsecl.impulse.core.gateway.GatewayModel;
-import com.github.impulsecl.impulse.core.gateway.GatewayRequestKind;
 import com.github.impulsecl.impulse.core.gateway.annotation.RequestMapping;
 import com.github.impulsecl.impulse.core.gateway.annotation.Route;
 
@@ -27,13 +26,8 @@ public class StandardGatewayCompiler implements GatewayModelCompiler {
           + "annotation to request the mapping");
     }
 
-    RequestMapping requestMapping = gatewayModel.getAnnotation(RequestMapping.class);
-
     for (Method method : gatewayModel.getDeclaredMethods()) {
       if (method.isAnnotationPresent(Route.class)) {
-        Route route = method.getAnnotation(Route.class);
-
-        GatewayRequestKind gatewayRequestKind = route.requestKind();
 
       }
     }
