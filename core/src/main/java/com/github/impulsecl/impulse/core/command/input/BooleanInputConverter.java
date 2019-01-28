6ffabd7d07d75736781c01fe51f74c13197654fd
ShadowAria <clockw1selrd@gmail.com>
2019-01-28 04:43:23 +0100

    Add null checks and annotation standards

--- core/src/main/java/com/github/impulsecl/impulse/core/command/input/BooleanInputConverter.java
+++ core/src/main/java/com/github/impulsecl/impulse/core/command/input/BooleanInputConverter.java
@@ -1,16 +1,26 @@
 package com.github.impulsecl.impulse.core.command.input;
 
+import com.github.impulsecl.impulse.common.semantic.Require;
+
+import edu.umd.cs.findbugs.annotations.CheckReturnValue;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
 import java.util.Collection;
 import java.util.List;
 
 public class BooleanInputConverter implements InputConverter<Boolean> {
 
+  @NonNull
   @Override
+  @CheckReturnValue
   public Boolean convert(String input) {
+    Require.requireParamNonNull(input, "input");
     return Boolean.parseBoolean(input);
   }
 
+  @NonNull
   @Override
+  @CheckReturnValue
   public Collection<Class<Boolean>> compatibleTypes() {
     return List.of(boolean.class, Boolean.class);
   }
