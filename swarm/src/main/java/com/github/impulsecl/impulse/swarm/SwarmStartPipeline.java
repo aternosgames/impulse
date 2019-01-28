1891009b322a90511fd5c49d3f5955ef229dc26b
Manuel Kollus <officialragecode@gmail.com>
2019-01-28 18:16:31 +0100

    Change value of annotation to '0'

--- swarm/src/main/java/com/github/impulsecl/impulse/swarm/SwarmStartPipeline.java
+++ swarm/src/main/java/com/github/impulsecl/impulse/swarm/SwarmStartPipeline.java
@@ -9,7 +9,7 @@ public class SwarmStartPipeline {
 
   private static final Logger LOGGER = LogManager.getLogger(SwarmStartPipeline.class);
 
-  @Stage(1)
+  @Stage(0)
   public void interpretSwarm() {
     LOGGER.info("Interpret the service 'swarm', please wait a moment...");
   }
