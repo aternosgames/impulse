4476b88251ba88963b20acd8278ab3dd7c8c527e
Manuel Kollus <officialragecode@gmail.com>
2019-01-28 22:57:08 +0100

    Add the 'parameters' annotation

--- core/src/main/java/com/github/impulsecl/impulse/core/gateway/annotation/Parameters.java
+++ core/src/main/java/com/github/impulsecl/impulse/core/gateway/annotation/Parameters.java
@@ -1,20 +1,14 @@
 package com.github.impulsecl.impulse.core.gateway.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
-@Repeatable(Parameters.class)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Parameter {
+public @interface Parameters {
 
-  String name();
-
-  Class<?> type();
-
-  boolean optional();
+  Parameter[] value();
 
 }
