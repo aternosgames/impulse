001353092d0448a72dfaa628a773194b26788863
Manuel Kollus <officialragecode@gmail.com>
2019-01-28 23:01:25 +0100

    Add 'Parameter' annotation and change structure of compiler class

--- core/src/main/java/com/github/impulsecl/impulse/core/gateway/compiler/StandardGatewayCompiler.java
+++ core/src/main/java/com/github/impulsecl/impulse/core/gateway/compiler/StandardGatewayCompiler.java
@@ -1,8 +1,10 @@
 package com.github.impulsecl.impulse.core.gateway.compiler;
 
+import com.github.impulsecl.impulse.common.semantic.Require;
 import com.github.impulsecl.impulse.core.gateway.GatewayException;
 import com.github.impulsecl.impulse.core.gateway.GatewayMethod;
 import com.github.impulsecl.impulse.core.gateway.GatewayModel;
+import com.github.impulsecl.impulse.core.gateway.annotation.Parameter;
 import com.github.impulsecl.impulse.core.gateway.annotation.RequestMapping;
 import com.github.impulsecl.impulse.core.gateway.annotation.Route;
 
@@ -11,7 +13,10 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StandardGatewayCompiler implements GatewayModelCompiler {
 
@@ -24,6 +29,8 @@ public class StandardGatewayCompiler implements GatewayModelCompiler {
   @NonNull
   @Override
   public GatewayModel compileAll(@NonNull Class<?> gatewayModelClass) {
+    Require.requireParamNonNull(gatewayModelClass, "gatewayModelClass");
+
     if (!gatewayModelClass.isAnnotationPresent(RequestMapping.class)) {
       throw new GatewayException("Cannot find the annotation '" + RequestMapping.class.getName() + "' please add the "
           + "annotation to request the mapping");
@@ -31,21 +38,50 @@ public class StandardGatewayCompiler implements GatewayModelCompiler {
 
     RequestMapping requestMapping = gatewayModelClass.getAnnotation(RequestMapping.class);
 
-    List<GatewayMethod> loadedGatewayMethods = new ArrayList<>();
-    for (Method method : gatewayModelClass.getDeclaredMethods()) {
-      if (method.isAnnotationPresent(Route.class)) {
-        Route route = method.getAnnotation(Route.class);
-
-        GatewayMethod gatewayMethod = GatewayMethod.create()
-            .route(route.name())
-            .gatewayRequestKind(route.requestKind());
-        loadedGatewayMethods.add(gatewayMethod);
-      }
-    }
+    List<GatewayMethod> loadedGatewayMethods = this.compileGatewayMethods(gatewayModelClass);
 
     return GatewayModel.create()
         .url(requestMapping.value())
         .gatewayMethods(loadedGatewayMethods);
+  }
+
+  @NonNull
+  @CheckReturnValue
+  private List<GatewayMethod> compileGatewayMethods(@NonNull Class<?> gatewayModelClass) {
+    Require.requireParamNonNull(gatewayModelClass, "gatewayModelClass");
+
+    List<GatewayMethod> loadedGatewayMethods = new ArrayList<>();
+
+    for (Method method : gatewayModelClass.getDeclaredMethods()) {
+      if (method.isAnnotationPresent(Route.class)) {
+        Route route = method.getAnnotation(Route.class);
+
+        Collection<Parameter> parameters = collectParameterAnnotations(method);
+
+        if (!(parameters.size() == method.getParameterCount())) {
+          throw new GatewayException(method.getDeclaringClass().getName()
+              + "#" + method.getName() + "'s parameter count does not match "
+              + "with the count of declared " + Parameter.class.getName() + " annotations");
+        }
+
+        GatewayMethod gatewayMethod = GatewayMethod.create()
+            .route(route.name())
+            .gatewayRequestKind(route.requestKind())
+            .method(method);
+        loadedGatewayMethods.add(gatewayMethod);
+      }
+    }
+
+    return loadedGatewayMethods;
+  }
+
+  @NonNull
+  @CheckReturnValue
+  private Collection<Parameter> collectParameterAnnotations(@NonNull Method method) {
+    Require.requireParamNonNull(method, "method");
+
+    return Arrays.stream(method.getDeclaredAnnotationsByType(Parameter.class))
+        .collect(Collectors.toList());
   }
 
 }
