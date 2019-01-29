c5542e338836d965b87f08b6de9aa7049abe2271
ShadowAria <clockw1selrd@gmail.com>
2019-01-29 23:56:11 +0100

    Add missing spotbugs annotations

--- core/src/main/java/com/github/impulsecl/impulse/common/input/InputConverter.java
+++ core/src/main/java/com/github/impulsecl/impulse/common/input/InputConverter.java
@@ -1,11 +1,18 @@
 package com.github.impulsecl.impulse.common.input;
 
+import edu.umd.cs.findbugs.annotations.CheckReturnValue;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
 import java.util.Collection;
 
 public interface InputConverter<T> {
 
+  @NonNull
+  @CheckReturnValue
   T convert(String input);
 
+  @NonNull
+  @CheckReturnValue
   Collection<Class<T>> compatibleTypes();
 
 }
