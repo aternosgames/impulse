7c4cf5840fad292d2a04de11c844e3f39c094e64
Manuel Kollus <officialragecode@gmail.com>
2019-01-30 19:24:08 +0100

    Wrap all classes with the copyright header

--- core/src/main/java/com/github/impulsecl/impulse/common/input/InputConverterRegistry.java
+++ core/src/main/java/com/github/impulsecl/impulse/common/input/InputConverterRegistry.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, Impulse and its contributors
+ *
+ * This code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package com.github.impulsecl.impulse.common.input;
 
 import edu.umd.cs.findbugs.annotations.CheckReturnValue;
